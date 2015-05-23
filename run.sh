6d0c5dd585dc7a5f365db3e73f2153635d5a07ee
tomo-chan <aradgyma@gmail.com>
2015-05-23 17:30:53 +0900

    Fix typo and change privilege

--- run.sh
+++ run.sh
@@ -6,9 +6,9 @@ if [ "$(`docker ps -a | grep -e " asterisk "`)" == "" ]; then
     echo Current directory is not empty.
     exit 1
   fi
-  docker run -dit --net=host --privileged \
+  docker run -d --net=host \
     -v `pwd`/etc/asterisk:/etc/asterisk \
-    -v `pwd`/var/log/asterisk:/var/log/astersik \
+    -v `pwd`/var/log/asterisk:/var/log/asterisk \
     --name=$IMAGE tomo-chan/asterisk
 fi
 
